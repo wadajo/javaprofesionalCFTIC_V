535674dec4b7499b81514c6b9ffae36d46ab5225
usuario <usuario@macbook-jc.home>
2020-10-09 09:33:29 +0200

    añadido isbn a los libros JSON

--- 47_libros_WS/src/util/Utilidades.java
+++ 47_libros_WS/src/util/Utilidades.java
@@ -10,24 +10,18 @@ import model.Libro;
 public class Utilidades {
 	public static JSONObject convertirLibroAJson(Libro unLibro) {
 		JSONObject objetoJson=new JSONObject();		
+		objetoJson.put("isbn", unLibro.getIsbn());
 		objetoJson.put("titulo", unLibro.getTitulo());
 		objetoJson.put("tematica", unLibro.getTema());
-		objetoJson.put("autor", unLibro.getAutor());
-		// quizás no interesa, no lo pongo | objetoJson.put("paginas", unLibro.getPaginas());
+		objetoJson.put("autor", unLibro.getAutor());		
 		objetoJson.put("precio", unLibro.getPrecio());
 		return objetoJson;
-	}
-	public static JSONObject[] convertirListaLibrosAJsonObject(List<Libro> libros) {
-		JSONObject[] arrayJson=new JSONObject[libros.size()];
-		for (Libro unLibro : libros) {
-			arrayJson[libros.indexOf(unLibro)]=(JSONObject) new JSONObject().put("titulo", unLibro.getTitulo());
-		}
-		return arrayJson;
-	}
+	}	
 	public static JSONArray convertirListaLibrosAJsonArray(List<Libro> libros) {
 		JSONArray arrayJson=new JSONArray();
 		for (Libro unLibro : libros) {
 			JSONObject unLibroEnJson=new JSONObject();		
+			unLibroEnJson.put("isbn", unLibro.getIsbn());
 			unLibroEnJson.put("titulo", unLibro.getTitulo());
 			unLibroEnJson.put("tematica", unLibro.getTema());
 			unLibroEnJson.put("autor", unLibro.getAutor());
